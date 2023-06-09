5f0f08c1823fedd847cad11f6f94fe5b250f91ff
Vinícius Gabriel Marques de Melo <vinicius_gabriel258@hotmail.com>
2023-06-08 23:56:53 -0300

    Apply several changes

--- KWIJisho.Database/Tables/Dictionary/Meaning.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Meaning]
-(
-	[Id] INT NOT NULL CONSTRAINT [PK_MeaningId] PRIMARY KEY IDENTITY(1,1), 
-  [Description] NCHAR(255) NOT NULL, 
-  [WordId] INT NOT NULL CONSTRAINT [FK_Meaning_Dictionary] REFERENCES [Word]([Id])
-)
--- KWIJisho.Database/Tables/Link.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Link]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Title] NVARCHAR(50) NULL, 
-    [Description] VARCHAR(255) NULL,
-    [Link] NCHAR(2048) NULL,
-)
