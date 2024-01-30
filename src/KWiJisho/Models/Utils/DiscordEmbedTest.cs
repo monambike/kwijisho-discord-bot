2539feb315969e660411c1c83911b22638237a11
Vinícius Gabriel Marques de Melo <vinicius_gabriel258@hotmail.com>
2024-01-30 00:04:06 -0300

    Create discord embed module

---
+++ src/KWiJisho/Models/Utils/DiscordEmbedTest.cs
@@ -0,0 +1,26 @@
+﻿using DSharpPlus.Entities;
+using System.IO;
+
+namespace KWiJisho.Models.Utils
+{
+    internal class DiscordEmbedTest
+    {
+        internal static DiscordEmbed GetDiscordEmbed(string title, string description, bool useKWiJishoThumbnail = false)
+        {
+            // Getting image name and image's full path
+            var fileName = $"256x256-ayaya.ico";
+            var imagePath = Path.GetFullPath($"{fileName}");
+
+            var thumbnail = (useKWiJishoThumbnail == true) ? new DiscordEmbedBuilder.EmbedThumbnail { Url = imagePath } : null;
+
+            var discordEmbedBuilder = new DiscordEmbedBuilder()
+            {
+                Color = ConfigJson.DefaultColor.DiscordColor,
+                Title = title, Description = description,
+                Thumbnail = thumbnail
+            };
+
+            return discordEmbedBuilder.Build();
+        }
+    }
+}
