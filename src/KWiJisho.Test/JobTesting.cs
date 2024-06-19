f72b402a436eb7d1380a243a3a725abf872f416a
Vinícius Gabriel <vinicius_gabriel258@hotmail.com>
2024-06-19 00:44:12 -0300

    Update birthday job unit testing

---
+++ src/KWiJisho.Test/JobTesting.cs
@@ -0,0 +1,24 @@
+using DSharpPlus;
+using KWiJisho.Utils;
+using Moq;
+using Quartz;
+
+namespace KWiJisho.Test
+{
+    public class JobTesting
+    {
+        [Fact]
+        public async Task Test1()
+        {
+            var mockLogs = new Mock<KWiJishoLog>(); // Mocka o KWiJishoLogs para simular seu comportamento
+            var mockDiscordClient = new Mock<DiscordClient>(); // Mocka o DiscordClient para simular seu comportamento
+            var mockContext = new Mock<IJobExecutionContext>(); // Mocka o IJobExecutionContext para simular seu comportamento
+            var dataMap = new JobDataMap
+            {
+                { "DiscordClient", mockDiscordClient.Object } // Configura o JobDataMap para retornar o mock do DiscordClient
+            };
+
+            mockContext.Setup(c => c.MergedJobDataMap).Returns(dataMap); // Configura o mockContext para retornar o JobDataMap 
+        }
+    }
+}
