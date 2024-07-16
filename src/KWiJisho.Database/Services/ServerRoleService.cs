e71c7d719e0ce3a7dfc5601bc51c2ee125ef1bda
Vinícius Gabriel <vinicius_gabriel258@hotmail.com>
2024-07-16 03:19:29 -0300

    Create ServerRoleService.cs

---
+++ src/KWiJisho.Database/Services/ServerRoleService.cs
@@ -0,0 +1,37 @@
+﻿// Copyright(c) 2024 Vinicius Gabriel Marques de Melo. All rights reserved.
+// Contact: @monambike for more information.
+// For license information, please see the LICENSE file in the root directory.
+
+using KWiJisho.Database.Data;
+
+namespace KWiJisho.Database.Services
+{
+    /// <summary>
+    /// Service class for managing operations related to ServerRole.
+    /// </summary>
+    public class ServerRoleService
+    {
+        /// <summary>
+        /// Asynchronously retrieves a ServerRole associated with the specified server ID.
+        /// </summary>
+        /// <param name="serverId">The ID of the server for which to retrieve the ServerRole.</param>
+        /// <returns>
+        /// A Task that represents the asynchronous operation. The task result contains 
+        /// the first matching ServerRole, or null if no match is found.
+        /// </returns>
+        public static async Task<ServerRole?> GetServerRolesByServerId(int serverId)
+        {
+            // Establish a connection to the SQLite database asynchronously.
+            var db = new Connection().SQLiteAsyncConnection;
+
+            // Query the ServerRole table for entries matching the specified serverId,
+            // and retrieve the results as a list asynchronously.
+            var result = await db.Table<ServerRole>()
+                .Where(serverRole => serverRole.ServerId == serverId)
+                .ToListAsync();
+
+            // Return the first matching ServerRole or null if no results are found.
+            return result.FirstOrDefault();
+        }
+    }
+}
