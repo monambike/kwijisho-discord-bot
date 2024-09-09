e10386ac26cc8679cf13e3d0f0115f98c6646136
Vinícius Gabriel <vinicius_gabriel258@hotmail.com>
2024-09-09 17:33:45 -0300

    Create Date utility class to help discord date format

---
+++ src/KWiJisho/Utils/UtilDate.cs
@@ -0,0 +1,39 @@
+﻿// Copyright(c) 2024 Vinicius Gabriel Marques de Melo. All rights reserved.
+// Contact: @monambike for more information.
+// For license information, please see the LICENSE file in the root directory.
+
+using System;
+using System.Globalization;
+
+namespace KWiJisho.Utils
+{
+    /// <summary>
+    /// Provides utility methods for working with dates.
+    /// </summary>
+    internal class UtilDate
+    {
+        /// <summary>
+        /// Returns the date in ISO 8601 date format.
+        /// </summary>
+        /// <param name="dateTime">The DateTime to be converted.</param>
+        /// <returns>A string containing a date in the ISO 8601 format.</returns>
+        public static string ConvertDateTimeToISO8601AsString(DateTime dateTime)
+            => dateTime.ToString("yyyy-MM-ddTHH\\:mm\\:ss.fffffffzzz", CultureInfo.InvariantCulture);
+
+        /// <summary>
+        /// Returns the date in UNIX date format.
+        /// </summary>
+        /// <param name="dateTime">The DateTime to be converted.</param>
+        /// <returns>A long containing a date in the UNIX format.</returns>
+        public static long ConvertDateTimeToUnixTimestamp(DateTime dateTime)
+        {
+            // Ensuring that the DateTime is in UTC.
+            var utcDateTime = dateTime.ToUniversalTime();
+
+            // Calculating the Unix timestamp.
+            long unixTimestamp = ((DateTimeOffset)utcDateTime).ToUnixTimeSeconds();
+
+            return unixTimestamp;
+        }
+    }
+}
