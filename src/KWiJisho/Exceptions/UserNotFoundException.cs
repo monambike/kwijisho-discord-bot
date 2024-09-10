dafc23694b845310fa0e82262a036dcdb8179fd4
Vinícius Gabriel <vinicius_gabriel258@hotmail.com>
2024-09-09 21:28:38 -0300

    Create UserNotFoundException.cs

---
+++ src/KWiJisho/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿// Copyright(c) 2024 Vinicius Gabriel Marques de Melo. All rights reserved.
+// Contact: @monambike for more information.
+// For license information, please see the LICENSE file in the root directory.
+
+using System;
+
+namespace KWiJisho.Exceptions
+{
+    internal class UserNotFoundException : Exception
+    {
+        public int NotFoundUserId;
+        public UserNotFoundException(string userId)
+            : base($"User with ID {userId} was not found.")
+        {
+
+        }
+    }
+}
