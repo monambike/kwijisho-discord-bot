5ea48f9180c836627418230d30aa68f0beaafb76
Vinícius Gabriel <vinicius_gabriel258@hotmail.com>
2024-09-09 21:17:15 -0300

    Update trigger documentation

--- data/Scripts/TR_InsertServer_DDL.sql
+++ data/Scripts/TR_InsertServer_DDL.sql
@@ -1,17 +1,14 @@
+CREATE TRIGGER TR_InsertServer_DDL
+
 /**************************************************************************************
 Copyright(c) 2024 Vinicius Gabriel Marques de Melo. All rights reserved.
 Contact: @monambike for more information.
 For license information, please see the LICENSE file in the root directory.
--
-Script created by @monambike. Please check https://github.com/monambike for more details,
-including the latest licensing information.
-
 **************************************************************************************/
 
 /* This trigger inserts default entries into tables that depend on the Server table
    whenever a new row is added to Server. */
 
-CREATE TRIGGER TR_InsertServer_DDL
 AFTER INSERT ON Server
 FOR EACH ROW
 BEGIN
@@ -20,4 +17,4 @@ BEGIN
 	
 	/* Inserting into the ServerRole table. */
 	INSERT INTO ServerRole (ServerId) VALUES (NEW.ServerId);
-END;
+END
