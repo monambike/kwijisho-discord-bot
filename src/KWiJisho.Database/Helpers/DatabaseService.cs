51ea7b466d4fc2989a45adbfa770fbd31dae78f1
Vinícius Gabriel <vinicius_gabriel258@hotmail.com>
2024-09-10 22:04:36 -0300

    Update DatabaseService.cs

--- src/KWiJisho.Database/Helpers/DatabaseService.cs
+++ src/KWiJisho.Database/Helpers/DatabaseService.cs
@@ -1,4 +1,8 @@
-﻿using KWiJisho.Database.Config;
+﻿// Copyright(c) 2024 Vinicius Gabriel Marques de Melo. All rights reserved.
+// Contact: @monambike for more information.
+// For license information, please see the LICENSE file in the root directory.
+
+using KWiJisho.Database.Config;
 using SQLite;
 
 namespace KWiJisho.Database.Helpers
